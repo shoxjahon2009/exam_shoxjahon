c178a2ef35cee65749f29d85bffbd1bbe9825fc3
Пользователь <shohjaxondehqonboyev59@gmail.com>
2024-01-20 14:00:32 +0500

    finished:13:59

---
+++ main.py
@@ -0,0 +1,69 @@
+# 1-masala boshlandi ::::
+
+# list = [1, 3, 4, 5, 6, 7, 8, 9, 10]
+# num = []
+# def tushib_qoldirilgan_son(list, num):
+#     for x in range(1, 10 + 1):
+#         if x not in list:
+#             num.append(x)
+#             print(x)
+# tushib_qoldirilgan_son(list, num)
+
+# 1-masla tugadi ::::
+
+
+# 2-masla boshlandi :::
+
+# def car(odamlar):
+#     moshina = 6
+#     cars_needed = (odamlar + moshina) // moshina
+#     return cars_needed
+#
+# total_people = int(input("kiriting: "))
+# cars = car(total_people)
+#
+# print(f"qilay moshina {total_people} dostlar siz uchun {cars} moshina.")
+
+# 2-masla tugadi ::::
+
+
+# 3 -masla boshlandi ::::
+
+# 3-maslani 1-chisi
+
+a = int(input("son:"))
+xisoblash = 1/2
+
+def calc():
+    c = a + xisoblash
+    print(f"hisobladi 50% ni kritgan son:{a} va javobi:{c}")
+calc()
+
+
+# 3-maslani 2-chisi
+
+# a = int(input("son:"))
+# xisoblash = 3/4
+#
+#
+# def calc():
+#     c = a + xisoblash
+#     print(f"hisobladi 75% ni kritgan son:{a} va javobi:{c}")
+# calc()
+
+
+
+# 3-maslani 3-chisi
+
+# a = int(input("son:"))
+# xisoblash = 9/20
+#
+# def calc():
+#     c = a + xisoblash
+#     print(f"hisobladi 45% ni kritgan son:{a} va javobi:{c}")
+# calc()
+
+
+
+
+# 3 -masla tugadi ::::
